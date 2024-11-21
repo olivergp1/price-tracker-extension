2dc71cc6f048a35526f372605e50f5f1d604fbd5
olivergp1 <oliver-gatland@hotmail.com>
2024-11-21 23:12:56 +0000

    Added new Firebase SDK files.

---
+++ firebase/firebase-app.js
@@ -0,0 +1,5 @@
+/**
+ * Firebase App (the core Firebase SDK) is always required and must be listed first.
+ */
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.17.1/firebase-app.js";
+export { initializeApp };
---
+++ firebase/firebase-firestore.js
@@ -0,0 +1,5 @@
+/**
+ * Firebase Firestore for database operations.
+ */
+import { getFirestore, doc, getDoc } from "https://www.gstatic.com/firebasejs/9.17.1/firebase-firestore.js";
+export { getFirestore, doc, getDoc };
